2f128d3359ca3737b73b9624e9dfe84b0002a846
ishansoni22 <soni.ishan.nitj@gmail.com>
2017-02-04 16:24:00 +0530

    master - register user's pages for activity subscription

--- requestHelper.js
+++ requestHelper.js
@@ -1,14 +1,38 @@
 "use strict";
 
-const rp = require('request-promise');
+const rq = require('request')
+const Task = require('data.task')
+const R = require('ramda')
+
+var hasError = R.has('error')
 
 module.exports = function(){
 
   var requestHelper = {
 
     request : function(options){
-        return rp(options)
-    }
+                return new Task(function(reject, resolve){
+                    rq(options, function(error, response, body){
+                        if(error){
+                            console.log("Error Occured while making a request for options", options, error)
+                            reject(error)
+                        } else {
+                           if(hasError(body)){
+                               console.log("Error Occured while making a request for options", options, body.error)
+                               reject(error)
+                           } else {
+                               var body = {
+                                   data : body,
+                                   config : options.config
+                               }
+                               resolve(body)
+                           }
+                        }
+                        
+                    })
+                })    
+            
+              }
 
   }
 
--- socialSubscribe.js
+++ socialSubscribe.js
@@ -1,7 +1,5 @@
 "use strict";
 
-//TODO Refactor code to use ramda properly after the first sign you see things working a little ;)
-
 const EventEmitter = require('events');
 const requestHelper = require('./requestHelper.js')()
 const R = require('ramda')
@@ -11,40 +9,64 @@ class socialSubscribeClass extends EventEmitter {}
 
 var socialSubscribe = new socialSubscribeClass()
 
-socialSubscribe.register = function(config){
-
-  var shortLivedAccessToken = config.getShortLivedAccessToken
-  var appID = config.getAppID
-  var appSecret = config.getAppSecret
-  //get long lived access token
-  var options = {}
-  var fbURI = graphApiHost.concat('/oauth/access_token?grant_type=fb_exchange_token&client_id=')
-                .concat(appID)
-                .concat('&client_secret=')
-                .concat(appSecret)
-                .concat('&fb_exchange_token=')
-                .concat(shortLivedAccessToken)
-
- var options = {
-   uri : fbURI
- }
-
- requestHelper.request(options).then(function(res) {
-   //TODO save the long lived access token to config object the other app is providing!
-   //config.setLongLivedAccessToken(res)
-
-
-
-
-
- }).catch(function(err){
-   console.log(err.message)
-   console.log(err.statusCode)
-
- })
-
+socialSubscribe.register = function(configInterface){
+    
+    register(configInterface).fork((error) => socialSubscribe.emit('registerFailedEvent', error), 
+                           (result) => socialSubscribe.emit('registerSuccessEvent', result))
 }
 
+
+var getLongLivedAccessToken = R.curry(function(host, config){
+    
+     var fbLongLivedAccessTokenURI = host.concat('/oauth/access_token?grant_type=fb_exchange_token&client_id=')
+                .concat(config.getAppID)
+                .concat('&client_secret=')
+                .concat(config.getAppSecret)
+                .concat('&fb_exchange_token=')
+                .concat(config.getShortLivedAccessToken)
+     
+     var options = {
+         uri : fbLongLivedAccessTokenURI ,
+         config : config
+     }
+    
+     return requestHelper.request(options)
+     
+})
+
+var setLongLivedFBAccessToken = function(body){
+    //TODO parse body properly
+    body.config.setLongLivedAccessToken(body.data)
+    //return updated config object
+    return body.config
+}
+
+var getUserPageDetails = R.curry(function(host, config){
+    
+    var pageDetailsAndTokensURI = host.concat("/me/accounts")
+    var options = {
+       uri : pageDetailsAndTokensURI,
+       qs: {
+                 access_token: config.getLongLivedAccessToken
+           },
+       config : config
+       
+   }
+    
+    return requestHelper.request(options)
+})
+
+var subscribePagesForActivity = R.curry(function(host, body){
+    var pages = JSON.parse(body.data)["data"]
+    //for each of the page, subscribe the given app to recieve updates on the callback url -
+    //TODO
+    return 1
+})
+
+var register = R.compose(R.map(subscribePagesForActivity(graphApiHost)),
+                            R.chain(getUserPageDetails(graphApiHost)),
+                                R.map(setLongLivedFBAccessToken),
+                                    getLongLivedAccessToken(graphApiHost))
 
 module.exports = function() {
 
